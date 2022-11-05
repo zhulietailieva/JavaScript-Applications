8c7286bf6ad800ad5f929e489a23c0995bbc0f4d
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-11-05 23:56:56 +0200

    Started woeking on Problem 2.

--- 02.AsynchronousProgramming/02.Bus-Schedule/app.js
+++ 02.AsynchronousProgramming/02.Bus-Schedule/app.js
@@ -1,11 +1,22 @@
 function solve() {
+    const url=`http://localhost:3030/jsonstore/bus/schedule/depot`;
+    const departBtn=document.getElementById('depart');
+    const arriveBtn=document.getElementById('arrive');
+    const infoEl=document.getElementById('info');
+    async function depart() {
+       let response =await fetch(url);
+       let data= await response.json();
+        infoEl.textContent=`Next stop ${data.name}`;
 
-    function depart() {
-        console.log('Depart TODO...');
+       departBtn.disabled='true';
+       arriveBtn.removeAttribute('disabled');
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoEl.textContent=`Arriving at ${data.name}`;
+
+        departBtn.removeAttribute('disabled');
+        arriveBtn.disabled='true';
     }
 
     return {
