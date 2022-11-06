ab64bb5bc4dcedbd7118cc29b5904346e6265bc0
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-11-06 21:26:39 +0200

    Solved problem 6 with result 75/100.

--- 02.AsynchronousProgramming/06.Blog/app.js
+++ 02.AsynchronousProgramming/06.Blog/app.js
@@ -1,5 +1,56 @@
 function attachEvents() {
-    console.log('TODO...');
+   const postsUrl='http://localhost:3030/jsonstore/blog/posts';
+   const commentsUrl='http://localhost:3030/jsonstore/blog/comments';
+
+   const postSelect=document.getElementById('posts');
+   const postsBtn=document.getElementById('btnLoadPosts')
+   .addEventListener('click',getPosts);
+   const viewBtn=document.getElementById('btnViewPost')
+   .addEventListener('click',showPost);
+
+   async function showPost(){
+            let response= await fetch(`${commentsUrl}`);
+            let data=await response.json();
+            //console.log(data);
+            let selectedCommentsArr=[];
+    for (const item of postSelect) {
+        if(item.selected){
+            let id=item.value;         
+          for (const comment of Object.entries(data)) {
+            if(comment[1].postId==id){
+                selectedCommentsArr.push(comment);
+            }
+          }
+          //array of json object with desired comments
+          let postTitleEl=document.getElementById('post-title');
+          postTitleEl.textContent=item.text;
+
+          let currPost=await fetch(`${postsUrl}/${id}`)
+          let currPostData=await currPost.json();
+
+          let postBodyEl=document.getElementById('post-body');
+          postBodyEl.textContent=currPostData.body;
+            
+          let postCommentsEl=document.getElementById('post-comments');
+          for (const comment of selectedCommentsArr) {
+            let li=document.createElement('li');
+           li.textContent=comment[1].text;
+           postCommentsEl.appendChild(li);
+          }
+        }
+    }
+   
+   }
+   async function getPosts(){
+    let response=await fetch(postsUrl);
+    let data=await response.json();
+    for (const item of Object.entries(data)) {
+        let option=document.createElement('option');
+        option.text=item[1].title;   
+        option.value=item[1].id;
+        postSelect.appendChild(option);    
+    }
+   }
 }
 
 attachEvents();
