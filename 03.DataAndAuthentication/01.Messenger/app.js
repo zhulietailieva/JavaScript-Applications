9be1c1fad4ee7c03f32e3d078d30f35570ac82e4
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-11-07 10:15:17 +0200

    Solved Problem 1.

--- 03.DataAndAuthentication/01.Messenger/app.js
+++ 03.DataAndAuthentication/01.Messenger/app.js
@@ -1,5 +1,35 @@
 function attachEvents() {
-    console.log('TODO...');
+    const initUrl='http://localhost:3030/jsonstore/messenger';  
+    let messages=document.getElementById('messages');
+
+    const postBtn=document.getElementById('submit')
+    .addEventListener('click',async ()=>{
+        let author=document.getElementsByTagName('input')[0].value;
+        let content=document.getElementsByTagName('input')[1].value;
+        let message={
+            author:author,
+            content:content
+        };
+        await fetch(initUrl,{
+            method:'post',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(message)
+        })
+    })
+    const refreshBtn=document.getElementById('refresh')
+    .addEventListener('click',async ()=>{
+        let response=await fetch(initUrl);
+        let data=await response.json();
+        let res=[];
+        for (const item of Object.values(data)) {
+            console.log(item);
+           let currUser=`${item.author}: ${item.content}`;
+            res.push(currUser);
+        }
+        messages.value=res.join('\n');
+    })
 }
 
 attachEvents();
