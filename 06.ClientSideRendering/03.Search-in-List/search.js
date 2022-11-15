8cf3aaa04147852c3ace57086b081cf216dccc7a
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-11-15 16:49:07 +0200

    Solved Problem 3.

--- 06.ClientSideRendering/03.Search-in-List/search.js
+++ 06.ClientSideRendering/03.Search-in-List/search.js
@@ -1,3 +1,30 @@
+import { html, render } from './node_modules/lit-html/lit-html.js';
+import { towns } from './towns.js';
+const townsEl = document.getElementById('towns');
+let template = html`
+<ul>
+${towns.map(town => html`
+<li>${town}</li>
+`)}
+</ul>
+`;
+render(template, townsEl);
+let searchBtn = document.getElementsByTagName('button')[0]
+   .addEventListener('click', search);
 function search() {
-   // TODO
+   let matches=0;
+   let searchText=document.getElementById('searchText').value;
+   let items=Array.from(townsEl.children[0].children);//li elements
+   items.forEach(item=>
+      {
+         if((item.textContent)
+         .includes(searchText)){
+            item.classList.add('active');
+            matches++;
+         }else{
+            item.classList.remove('active');
+         }
+      });
+   let resEl=document.getElementById('result');
+   resEl.textContent= `${matches} matches found`;
 }
