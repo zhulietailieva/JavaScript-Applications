a160515fa4ae1154388fb562c3db083de1f8e17c
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-11-15 22:10:11 +0200

    Started working on Problem 6.

--- 06.ClientSideRendering/06.Book-Library/app.js
+++ 06.ClientSideRendering/06.Book-Library/app.js
@@ -1,1 +1,4 @@
-console.log('My requests...')
+import {html,render} from './node_modules/lit-html/lit-html.js';
+
+let response= await fetch('http://localhost:3030/jsonstore/collections/books');
+let data =await response.json();
