7aa1f7c49cc7f4f241e51a43071988851e6fddd6
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-11-15 15:15:59 +0200

    Solved Problem 2.

--- 06.ClientSideRendering/02.HTTP-Status-Cats/app.js
+++ 06.ClientSideRendering/02.HTTP-Status-Cats/app.js
@@ -1,4 +1,31 @@
-function e(){
-    console.log('hi');
+import {html,render} from './node_modules/lit-html/lit-html.js'
+import{cats} from './catSeeder.js';
+const catsSection=document.getElementById('allCats');
+let catTemplate=html`
+<ul>
+${cats.map(cat=>html`
+<li>
+<img src="./images/${cat.imageLocation}.jpg" width="250" height="250" alt="Card image cap">
+<div class="info">
+<button @click=${onClick} class="showBtn">Show status code</button>
+<div class="status" style="display: none" id="${cat.id}}">
+    <h4>Status Code: ${cat.statusCode}</h4>
+    <p>${cat.statusMessage}</p>
+</div>
+</div>
+</li>
+`)}
+</ul>
+`
+render(catTemplate,catsSection);
+function onClick(e){
+    if(e.target.textContent=="Show status code"){
+        e.target.textContent="Hide status code";
+        e.target.parentElement.children[1].style.display="block";
+    }else {
+        e.target.target="Show status code";
+        e.target.parentElement.children[1].style.display="none";
+    }
 }
-export {e}
+
+
