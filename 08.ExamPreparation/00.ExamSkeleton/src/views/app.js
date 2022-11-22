df3cd054d9d6114294e710b533941aa13f117409
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-11-22 16:12:32 +0200

    Made some changes to the Skeleton files.

--- 08.ExamPreparation/00.ExamSkeleton/src/views/app.js
+++ 08.ExamPreparation/00.ExamSkeleton/src/views/app.js
@@ -1,3 +1,7 @@
-import * as api from '../api/users.js';
+import {page,render} from './lib.js';
+//import * as api from '../api/users.js';
+//window.api = api;
 
-window.api = api;
+page('/',()=>console.log('home'));
+//Start application
+page.start();
