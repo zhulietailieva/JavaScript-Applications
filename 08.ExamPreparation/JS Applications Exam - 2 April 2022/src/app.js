9f1c31098b8c708d67bff0b02fb3f274dd14e711
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-11-28 17:42:30 +0200

    Started working on the Pet Care logic.

---
+++ 08.ExamPreparation/JS Applications Exam - 2 April 2022/src/app.js
@@ -0,0 +1,26 @@
+import { render, page } from './lib.js';
+import { showCatalog } from './views/catalog.js';
+import { showHome } from './views/home.js';
+import { showLogin } from './views/login.js';
+
+const main = document.getElementById('content');
+
+page(decorateContext);
+page('/', showHome);
+page('/catalog', showCatalog);
+page('/catalog/:id', () => console.log('details'));
+page('/edit/:id', () => console.log('edit'));
+page('/create', () => console.log('create'));
+page('/login', showLogin);
+page('/register', () => console.log('register'));
+
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = renderMain;
+    next();
+}
+
+function renderMain(content) {
+    render(content, main);
+}
